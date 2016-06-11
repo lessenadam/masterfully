9c451037464dea5bc9f3aa20ca70d84dfcc7b2c2
Adam Lessen <lessena@gmail.com>
2016-06-10 17:25:56 -0700

    Change to real d3 inside of react using real data

--- client/components/sessions-view/line.jsx
+++ client/components/sessions-view/line.jsx
@@ -4,8 +4,9 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 // var Chart = require('react-d3-core').Chart;
 // var LineChart = require('react-d3-basic').LineChart;
-import rd3 from 'rd3';
-var LineChart = rd3.LineChart;
+// import rd3 from 'rd3';
+// var LineChart = rd3.LineChart;
+import $ from 'jquery';
 
 
 export default class Chart extends React.Component {
@@ -13,37 +14,82 @@ export default class Chart extends React.Component {
     super(props);
     this.state = {
       chartData: this.props.data,
-      lineData:[
-        { 
-          name: 'Performance',
-          values: [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 }, { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ],
-          strokeWidth: 3        }
-      ]
+      width: 800,
+      height: 400,
+      chartId: 'v1_chart',
     };
   }
 
+  componentDidMount() {
+    console.log('this.props.data is: ', this.state.chartData);    
+    console.log('this.props.plainData is: ', this.state.plainData);
+    // this.setState({
+    //   width:this.props.width
+    // })
+
+  } 
 
   render() {
+    var margin = {top: 5, right: 50, bottom: 20, left: 50},
+        w = this.state.width - (margin.left + margin.right),
+        h = this.state.height - (margin.top + margin.bottom);
+
+    var x = d3.scale.linear()
+        .domain([0,d3.max(this.state.chartData,function(d){
+            return d.index+4;
+        })])
+        .rangeRound([0, w]);
+ 
+    // updated!
+    var y = d3.scale.linear()
+        .domain([0,d3.max(this.state.chartData,function(d){
+            return d.score+4;
+        })])
+        .range([h, 0]);
+
+    // updated!
+    var line = d3.svg.line()
+        .x(function (d) {
+            console.log('index: ',d.index);
+            console.log('score: ',d.score);
+
+            return x(d.index);
+        })
+        .y(function (d) {
+            return y(d.score);
+        }).interpolate('cardinal');
+
+    var transform='translate(' + margin.left + ',' + margin.top + ')';
+
     return (
-      <LineChart
-        legend={true}
-        data={this.state.lineData}
-        width='80%'
-        height={300}
-        viewBoxObject={{
-          x: 0,
-          y: 0,
-          width: 500,
-          height: 300
-        }}
-        title="Performance Over Time"
-        yAxisLabel="Performance"
-        xAxisLabel="Session#"
-        domain={{x: [,6], y: [-10,]}}
-        gridHorizontal={true}
-      />
-    )
+          <div>
+            <svg id={this.props.chartId} width={this.state.width} height={this.state.height}>
+              <g transform={transform}>
+                <path className="session-line session-shadow" d={line(this.state.chartData)} strokeLinecap="round"/>
+                <Dots data={this.state.chartData} x={x} y={y}/>
+              </g>
+            </svg>
+          </div>
+        );
   }
 };
 
+var Dots = (props) => {
+  var _self=this;
+ 
+        //remove last & first point
+  var data=props.data.splice(1);
+  data.pop();
+
+  var circles=data.map(function(d,i){
+
+    return (<circle className="dot" r="7" cx={props.x(d.index)} cy={props.y(d.score)} fill="#7dc7f4" stroke="#3f5175" strokeWidth="5px" key={i} />);
+  });
+
+  return(
+      <g>
+        {circles}
+      </g>
+  );
+};
 
